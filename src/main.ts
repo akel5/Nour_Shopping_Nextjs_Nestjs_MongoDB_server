4c8fae39083adc6a4000ade4ba87fe4447350e01
akel5 <akel.obied@outlook.com>
2025-11-29 11:34:30 +0200

    adding CORS for external server

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express'; // 1. ייבוא הטיפוס של Express
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors(); // <--- הוסף את השורה הזאת
+  // 2. הוספת הטיפוס הגנרי <NestExpressApplication> כדי ש-TypeScript יכיר את פקודות Express
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // 1. הפעלת CORS בצורה מתירנית (מאפשרת לכל דומיין לגשת)
+  app.enableCors({
+    origin: true, // מאפשר לכל מקור (Vercel) לגשת
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  // 2. אבטחה בסיסית וולידציה
+  app.disable('x-powered-by'); // עכשיו זה תקין כי הגדרנו שזו אפליקציית Express
   app.useGlobalPipes(new ValidationPipe());
+
+  // 3. האזנה לפורט של Render או ל-3001
   await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
